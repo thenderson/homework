9db4a352f1dd98ac7634822e8b56d6fb3bd1cb18
thenderson <todd.hndrsn@gmail.com>
2014-06-29 12:57:36 -0700

    more sql tweaks

--- public/index.php
+++ public/index.php
@@ -10,18 +10,16 @@
     //TODO ability to update status, description, etc.
     //TODO ability to sort & filter
 	error_log("form was posted!");
-		$now = new DateTime();
-        $planning_horizon = $now->modify("+14 day");
-        $commitments = query("SELECT * FROM commitments WHERE due_by <= ? ORDER BY promisor, project, due_by", $planning_horizon);
+        $planning_horizon = 14;
+        $commitments = query("SELECT * FROM commitments WHERE due_by <= DATE_ADD(CURDATE(),INTERVAL ? DAY) ORDER BY promisor, project, due_by", $planning_horizon);
         render("commitments_form.php", ["commitments" => $commitments]);
     }
     else // show default configuration
     {
     //TODO save user preferences for setup; show only user's projects
 	error_log("showing default config");
-		$now = new DateTime();
-        $planning_horizon = $now->modify("+28 day");
-        $commitments = query("SELECT * FROM commitments WHERE due_by <= ? ORDER BY project, due_by, promisor", $planning_horizon);
+        $planning_horizon = 28;
+        $commitments = query("SELECT * FROM commitments WHERE due_by <= DATE_ADD(CURDATE(),INTERVAL ? DAY) ORDER BY project, due_by, promisor", $planning_horizon);
         render("commitments_form.php", ["commitments" => $commitments]);
     }
 ?>
