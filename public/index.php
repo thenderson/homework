7a6d28707b25a518da3b4c7bebfa59adb10e4ca9
thenderson <todd.hndrsn@gmail.com>
2014-06-29 22:55:43 -0700

    bugs stomping

--- public/index.php
+++ public/index.php
@@ -3,17 +3,17 @@
     // configuration
     require("../includes/config.php");
 
-    // if form was submitted
-    if ($_SERVER["REQUEST_METHOD"] == "POST")
-    {
+    //if form was submitted
+    // if ($_SERVER["REQUEST_METHOD"] == "POST")
+    // {
     //TODO adjustable filters for projects, planning horizon, status & type
     //TODO ability to update status, description, etc.
     //TODO ability to sort & filter
 
         render("commitments_form.php");
-    }
-    else // else ... huh. Is there an else?
-    {
-		apologize("hmmm. something odd in index.php.");
-    }
+    // }
+    // else // else ... huh. Is there an else?
+    // {
+		// apologize("hmmm. something odd in index.php.");
+    // }
 ?>
