d29a15bddb83259421b787d385829f26f6ab9a03
thenderson <todd.hndrsn@gmail.com>
2014-07-02 19:12:09 -0700

    m

--- templates/commitments_form.php
+++ templates/commitments_form.php
@@ -2,77 +2,99 @@
 
 	<h5>C O M M I T M E N T S</h5>
 
+	<div class="alert alert-error hide">
+		That would cost too much
+	</div>
+
 	<table class="table table-striped" id="commitments">
-		<tr>
-			<th>id</th>
-			<th>description</th>
-			<th>promiser</th>
-			<th>requester</th>
-			<th>due</th>
-			<th>status</th>
-		</th>
-		
-		<?php
+		<thead>
+			<tr>
+				<th>project #</th>
+				<th>project name</th>
+				<th>task #</th>
+				<th>description</th>
+				<th>promiser</th>
+				<th>requester</th>
+				<th>due by</th>
+				<th>days</th>
+				<th>requested on</th>
+				<th>status</th>
+				<th>type</th>
+				<th>metric</th>
+			</th>
+		</thead>
+	
+		<tbody>
+			<?php
 
-		$now = new DateTime();
-		
-		foreach ($commitments as $commitment)
-		{
-			$days_til_due = date_diff($now, new DateTime($commitment["due_by"]))->days;
+			$now = new DateTime();
 			
-			switch($days_til_due) //choose row formatting by task due date proximity
+			foreach ($commitments as $commitment)
 			{
-				case($days_til_due<0):
-					?>
-					<tr class="danger">
-					<?php
-					break;
-				case($days_til_due<8):
-					?>
-					<tr class="info">
-					<?php
-					break;
-				default:
-					?>
-					<tr>
-					<?
-			}   
-			
-		?>
-
-				<td><?= $commitment["task_id"]?></td>
-				<td><?= $commitment["description"]?></td>
-				<td><?= $commitment["requester"]?></td>
-				<td><?= $commitment["promiser"]?></td>
-				<td><?= $commitment["due_by"]?></td>
-				<td><?= $commitment["status"]?></td>
-			</tr>
-		<?php      
-		}
-		?>
-
+				$days_til_due = date_diff($now, new DateTime($commitment["due_by"]))->days;
+				
+				switch($days_til_due) //choose row formatting by task due date proximity
+				{
+					case($days_til_due<0):
+						?>
+						<tr class="danger">
+						<?php
+						break;
+					case($days_til_due<8):
+						?>
+						<tr class="info">
+						<?php
+						break;
+					default:
+						?>
+						<tr>
+						<?
+				}   
+				
+			?>
+					<td><?= $commitment["project"]?></td>
+					<td><?= $commitment["project"]?></td>
+					<td><?= $commitment["task_id"]?></td>
+					<td><?= $commitment["description"]?></td>
+					<td><?= $commitment["requester"]?></td>
+					<td><?= $commitment["promiser"]?></td>
+					<td><?= $commitment["due_by"]?></td>
+					<td><?= $days_til_due?></td>
+					<td><?= $commitment["status"]?></td>
+					<td><?= $commitment["type"]?></td>
+					<td><?= $commitment["metric"]?></td>
+				</tr>
+			<?php      
+			}
+			?>
+		</tbody>
     </table>
+	<tfoot>
+		<tr>
+			
+		</tr>
+	</tfoot>
 	
     <script>
-	$('#commitments').editableTableWidget();
-	$('#commitments').editableTableWidget({editor: $('<textarea>')});
-	$('#commitments').editableTableWidget({
-	cloneProperties: ['background', 'border', 'outline']
-	});
-	
-	<!-- mark invalid data -->
-	//$('table td').on('validate', function(evt, newValue) {
-	//	if (....) { 
-	//		return false; // mark cell as invalid 
-	//	}
-	//});
+		$('#commitments').editableTableWidget();
+		$('#commitments').editableTableWidget({editor: $('<textarea>')});
+		$('#commitments').editableTableWidget({
+		cloneProperties: ['background', 'border', 'outline']
+		});
+		
+		<!-- mark invalid data -->
+		//$('table td').on('validate', function(evt, newValue) {
+		//	if (....) { 
+		//		return false; // mark cell as invalid 
+		//	}
+		//});
 
-	<!-- act on changed data -->
-	//$('table td').on('change', function(evt, newValue) {
-	// do something with the new cell value 
-	//if (....) { 
-	//	return false; // reject change
-	//}
-	//});
+		<!-- act on changed data -->
+		//$('table td').on('change', function(evt, newValue) {
+		// do something with the new cell value 
+		//if (....) { 
+		//	return false; // reject change
+		//}
+		//});
     </script>
 </div>
