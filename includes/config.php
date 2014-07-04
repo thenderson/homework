2c9fd78985e6eef89dfdcfca95e8ac03494a3f1c
thenderson <todd.hndrsn@gmail.com>
2014-07-03 23:14:37 -0700

    b

--- includes/config.php
+++ includes/config.php
@@ -28,11 +28,10 @@
             redirect("login.php");
         }
     }
-	echo ("bing!");
 	// open connection to sql database
 	$comm_db = new mysqli("SERVER", "USERNAME", "PASSWORD", "DATABASE");
 	if ($comm_db->connect_errno) {
-		echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+		echo "Failed to connect to MySQL: (" . $comm_db->connect_errno . ") " . $comm_db->connect_error;
 		}
 
 ?>
