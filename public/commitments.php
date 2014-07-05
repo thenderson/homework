560543e8726cc465ca504591452078f8661ac4a7
thenderson <todd.hndrsn@gmail.com>
2014-07-04 23:38:54 -0700

    m

--- public/commitments.php
+++ public/commitments.php
@@ -8,8 +8,8 @@
     //TODO ability to sort & filter
 
         $planning_horizon = 14;
-        $commitments = $comm_db->query("SELECT * FROM commitments WHERE due_by <= DATE_ADD(CURDATE(), INTERVAL {$planning_horizon} DAYS), ORDER BY promiser, project_number, due_by");
+        $commitments = $comm_db->query("SELECT * FROM commitments WHERE due_by <= DATE_ADD(CURDATE(), INTERVAL $planning_horizon DAY), ORDER BY promiser, project_number, due_by");
         var_dump($commitments);
-		var_dump($comm_db);
+		var_dump($comm_db["error"]);
 		render("commitments_form.php", ["commitments" => $commitments]);
 ?>
