12b0372b14dc23c27912ec7cb5f938a3b9840f53
thenderson <todd.hndrsn@gmail.com>
2014-07-04 19:28:57 -0700

    m

--- includes/functions.php
+++ includes/functions.php
@@ -65,12 +65,12 @@
 			
 			$bt = debug_backtrace();
 			$caller = array_shift($bt);
-			$file_line = "&lt;strong&gt;" . $caller['file'] . "(line " . $caller['line'] . ")&lt;/strong&gt;\n";
+			$file_line = "<strong>" . $caller['file'] . "(line " . $caller['line'] . ")</strong>\n";
 			
 			if(!$r)
 			{ //if print
 				echo '<pre>';
-				echo '&lt;!--Debugger Line: ' . $file_line . '--&gt;' . $bt[];
+				echo '&lt;!--Debugger Line: ' . $file_line . '--&gt;' . $bt;
 				print_r($file_line);
 				$func($p);
 				echo '</pre>';
