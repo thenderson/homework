02e110e3a6306d897e15e3eeb61d2d8c82b9baa3
thenderson <todd.hndrsn@gmail.com>
2014-07-06 12:49:37 -0700

    m
    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/commitments.php
+++ public/commitments.php
@@ -38,8 +38,8 @@
 	}
 	else
 	{
-//		for ($users = array (); $row = $users_res->fetch_assoc(); $users[] = $row);
-		for ($users = array (); $row = $users_res->fetch_assoc(); $users[array_shift($row)] = $row);
+//		for ($users = array (); $row = $users_res->fetch_assoc(); $users[] = $row); //makes associative array
+		for ($users = array (); $row = $users_res->fetch_assoc(); $users[array_shift($row)] = $row); //makes array keyed to first field
 	}
 	
 	/*	RETRIEVE PROJECT NUMBERS & PROJECT SHORTNAMES */ //move this to config & pass into this script?
@@ -51,10 +51,12 @@
 	}
 	else
 	{
-		for ($projects = array (); $row = $projects_res->fetch_assoc(); $projects[] = $row);
+		for ($projects = array (); $row = $projects_res->fetch_assoc(); $projects[array_shift($row)] = $row);
 	}
 
-		dbug('$users_res', $users_res, '$users', $users, '$projects_res', $projects_res, '$projects', $projects); 
-		echo dbug('print');
+	dbug('$project_numbers', $project_numbers, '$sask_ids', $task_ids, '$descriptions', $descriptions, '$requesters', $requesters, '$promisers', $promisers, '$due_bys', $due_bys, '$requested_ons', $requested_ons, '$statuses', $statuses, '$types', $types, '$metrics', $metrics);
+	dbug('$users_res', $users_res, '$users', $users); 
+	dbug('$projects_res', $projects_res, '$projects', $projects); 
+	echo dbug('print');
 
-//	render("commitments_form.php", ["project_numbers"=>$project_numbers, "task_ids"=>$task_ids, "descriptions"=>$descriptions, "requesters"=>$requesters, "promisers"=>$promisers, "due_bys"=>$due_bys, "requested_ons"=>$requested_ons, "statuses"=>$statuses, "types"=>$types, "metrics"=>$metrics, "users" => $users, "projects" => $projects]);
+	render("commitments_form.php", ["project_numbers"=>$project_numbers, "task_ids"=>$task_ids, "descriptions"=>$descriptions, "requesters"=>$requesters, "promisers"=>$promisers, "due_bys"=>$due_bys, "requested_ons"=>$requested_ons, "statuses"=>$statuses, "types"=>$types, "metrics"=>$metrics, "users" => $users, "projects" => $projects]);
