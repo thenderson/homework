3c29e34d1213b22d46312f6e0c6abf73c28a12b3
thenderson <todd.hndrsn@gmail.com>
2014-07-05 23:09:43 -0700

    m

--- public/commitments.php
+++ public/commitments.php
@@ -45,9 +45,9 @@
 		trigger_error('Statement failed : ' . E_USER_ERROR);
 	}
 
-        var_dump($project_numbers);
-		var_dump($task_ids);
-		var_dump($users);
-		var_dump($projects);
+        pr($project_numbers, 'var_dump');
+		pr($task_ids, 'var_dump');
+		pr($users, 'var_dump');
+		pr($projects, 'var_dump');
 
 	render("commitments_form.php", ["project_numbers"=>$project_numbers, "task_ids"=>$task_ids, "descriptions"=>$descriptions, "requesters"=>$requesters, "promisers"=>$promisers, "due_bys"=>$due_bys, "requested_ons"=>$requested_ons, "statuses"=>$statuses, "types"=>$types, "metrics"=>$metrics, "users" => $users, "projects" => $projects]);
