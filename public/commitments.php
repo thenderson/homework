96a79221a4f61f6f0d0a38e21364cca3fc8e0372
thenderson <todd.hndrsn@gmail.com>
2014-07-06 15:36:54 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/commitments.php
+++ public/commitments.php
@@ -34,32 +34,8 @@
 		trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $e->getMessage(), E_USER_ERROR);
 	}
 	
-	dbug('$comm_db', $comm_db);
-	dbug('$stmt', $stmt);
-	dbug('$commitments', $commitments);
-	echo dbug('print');
-	exit;
-//		$stmt->bind_param("i", $planning_horizon);
-//		$stmt->execute();
-//		$stmt->store_result();
-//		$stmt->bind_result($proj, $taskid, $desc, $req, $prom, $due, $req_ons, $stat, $types, $met);
-//		$stmt->fetch();
-//		$stmt->close();
-		
-		// for ($project_numbers = array (); $row = $proj->fetch_assoc(); $project_numbers[] = $row);
-		// for ($task_ids = array (); $row = $taskid->fetch_assoc(); $task_ids[] = $row);
-		// for ($descriptions = array (); $row = $desc->fetch_assoc(); $descriptions[] = $row);
-		// for ($requesters = array (); $row = $req->fetch_assoc(); $requesters[] = $row);
-		// for ($promisers = array (); $row = $prom->fetch_assoc(); $promisers[] = $row);
-		// for ($due_bys = array (); $row = $due->fetch_assoc(); $due_bys[] = $row);
-		// for ($requested_ons = array (); $row = $req_ons->fetch_assoc(); $requested_ons[] = $row);
-		// for ($statuses = array (); $row = $stat->fetch_assoc(); $statuses[] = $row);
-		// for ($types = array (); $row = $types->fetch_assoc(); $types[] = $row);
-		// for ($metrics = array (); $row = $met->fetch_assoc(); $metrics[] = $row);
-	
-	
 	/*	RETRIEVE USERNAMES & EMAIL ADDRESSES */ //move this to config & pass into this script?
-	$users_res = $comm_db->query("SELECT email, name FROM users ORDER BY email ASC");
+	$users_res = $comm_db->query("SELECT email, name FROM users ORDER BY email ASC", PDO::FETCH_ASSOC);
 	
 	if (!$users_res)
 	{
@@ -67,12 +43,12 @@
 	}
 	else
 	{
-		for ($users = array (); $row = $users_res->fetch_assoc(); $users[] = $row); //makes associative array
+//		for ($users = array (); $row = $users_res->fetch_assoc(); $users[] = $row); //makes associative array
 //		for ($users = array (); $row = $users_res->fetch_assoc(); $users[array_shift($row)] = $row); //makes array keyed to first field
 	}
 	
 	/*	RETRIEVE PROJECT NUMBERS & PROJECT SHORTNAMES */ //move this to config & pass into this script?
-	$projects_res = $comm_db->query("SELECT project_number, project_shortname FROM projects");
+	$projects_res = $comm_db->query("SELECT project_number, project_shortname FROM projects", PDO::FETCH_ASSOC);
 	
 	if (!$projects_res)
 	{
@@ -80,11 +56,10 @@
 	}
 	else
 	{
-		for ($projects = array (); $row = $projects_res->fetch_assoc(); $projects[] = $row);  //makes associative array
+//		for ($projects = array (); $row = $projects_res->fetch_assoc(); $projects[] = $row);  //makes associative array
 //		for ($projects = array (); $row = $projects_res->fetch_assoc(); $projects[array_shift($row)] = $row);  //makes array keyed to first field
 	}
-
-//	dbug('$project_numbers', $project_numbers, '$task_ids', $task_ids, '$descriptions', $descriptions, '$requesters', $requesters, '$promisers', $promisers, '$due_bys', $due_bys, '$requested_ons', $requested_ons, '$statuses', $statuses, '$types', $types, '$metrics', $metrics);
+	
 	dbug('$commitments', $commitments);
 	dbug('$users', $users); 
 	dbug('$projects', $projects); 
