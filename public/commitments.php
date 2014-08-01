3665a4cb12561a19047267e98467b7fb876032c9
thenderson <todd.hndrsn@gmail.com>
2014-08-01 15:14:18 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/commitments.php
+++ public/commitments.php
@@ -59,4 +59,4 @@
 
 //	render("commitments_form.php", ["commitments"=>$commitments, "users" => $users, "username_lookup"=>$username_lookup, "projects" => $projects]);
 
-	require("../templates/commitments_form.php");
+	require("../templates/commitments_form.html");
