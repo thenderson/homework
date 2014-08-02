6a7aad4161623a0a57541cb165ed903ad23443d3
thenderson <todd.hndrsn@gmail.com>
2014-08-02 11:45:19 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/commitments.php
+++ public/commitments.php
@@ -59,5 +59,5 @@
 
 //	render("commitments_form.php", ["commitments"=>$commitments, "users" => $users, "username_lookup"=>$username_lookup, "projects" => $projects]);
 
-	redirect("../templates/commitments_form.php");
+	redirect("../templates/commitments_form.php?");
 	exit;
