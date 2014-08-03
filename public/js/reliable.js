1edd1163ca8c59895dcd8c8f38a65c79001acada
thenderson <todd.hndrsn@gmail.com>
2014-08-03 12:53:48 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/js/reliable.js
+++ public/js/reliable.js
@@ -149,8 +149,8 @@ DatabaseGrid.prototype.initializeGrid = function(grid) {
 	grid.setCellRenderer('action', new CellRenderer({ 
 		render: function(cell, id) {                 
 		     cell.innerHTML+= "<i onclick=\"datagrid.deleteRow("+id+");\" class='fa fa-trash-o' ></i>";
-			 CellRenderer.prototype.render.call(this, cell, value);
-			$(cell).addClass('metric');
+			// CellRenderer.prototype.render.call(this, cell, value);
+			//$(cell).addClass('metric');
 		}
 	})); 
 }
