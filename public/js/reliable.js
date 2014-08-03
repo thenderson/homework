c4e09435edbab09b4e05fe9c0d394cdbd71d50b2
thenderson <todd.hndrsn@gmail.com>
2014-08-03 15:55:37 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/js/reliable.js
+++ public/js/reliable.js
@@ -211,9 +211,9 @@ DatabaseGrid.prototype.initializeGrid = function(grid) {
 	// renderers for the action column
 	grid.setCellRenderer('actions', new CellRenderer({ 
 		render: function(cell, id) {                 
-		     cell.innerHTML+= "<i onclick=\"datagrid.deleteRow("+id+");\" class='fa fa-trash-o' ></i>";
-			// CellRenderer.prototype.render.call(this, cell, value);
-			//$(cell).addClass('metric');
+		    cell.innerHTML+= "<i onclick=\"datagrid.deleteRow("+id+");\" class='fa fa-trash-o' ></i>";
+			CellRenderer.prototype.render.call(this, cell, value);
+			$(cell).addClass('actions');
 		}
 	})); 
 		
