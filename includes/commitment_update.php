f0b2ff9053e1a802577cd77a2de4ea5a0763fdff
thenderson <todd.hndrsn@gmail.com>
2014-08-17 22:57:34 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- includes/commitment_update.php
+++ includes/commitment_update.php
@@ -9,10 +9,10 @@
 require_once('config.php');         
                       
 // Get all parameters provided by the javascript
-$unique_id = $mysqli->real_escape_string(strip_tags($_POST['uniqueid']));
-$new_value = $mysqli->real_escape_string(strip_tags($_POST['newvalue']));
-$column_name = $mysqli->real_escape_string(strip_tags($_POST['colname']));
-$column_type = $mysqli->real_escape_string(strip_tags($_POST['coltype']));
+$unique_id = $comm_db->real_escape_string(strip_tags($_POST['uniqueid']));
+$new_value = $comm_db->real_escape_string(strip_tags($_POST['newvalue']));
+$column_name = $comm_db->real_escape_string(strip_tags($_POST['colname']));
+$column_type = $comm_db->real_escape_string(strip_tags($_POST['coltype']));
                                                 
 // spruce-up the date format
 if ($column_type == 'date') {
