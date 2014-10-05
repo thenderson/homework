cb375581899b8d31e59206eae3bef83b11e3a4d1
thenderson <todd.hndrsn@gmail.com>
2014-10-04 23:03:47 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/commitments.php
+++ public/commitments.php
@@ -12,7 +12,7 @@
 	$planning_horizon = 14; // days
 	
 	$stmt = $comm_db->prepare("
-		SELECT unique_id, project_number, task_id, description, requester, promiser, due_by, requested_on, status, type, metric 
+		SELECT unique_id, project_number, task_id, description, requester, promiser, DATE_FORMAT(due_by,'%d/%m/%y'), DATE_FORMAT(requested_on, '%d/%m/%y'), status, type, metric 
 		FROM commitments 
 		WHERE due_by <= DATE_ADD(CURDATE(), INTERVAL ? DAY) 
 		ORDER BY project_number, promiser, due_by");
