d353b3671c10789d959f5c170c9a107ae641e704
thenderson <todd.hndrsn@gmail.com>
2014-10-04 22:18:07 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/js/reliable.js
+++ public/js/reliable.js
@@ -28,6 +28,7 @@ function DatabaseGrid()
 	
 	this.editableGrid = new EditableGrid("grid", {
 		enableSort: true,
+		dateFormat: "US",
 	    // define the number of row visible by page
       	pageSize: 10,
       // Once the table is displayed, we update the paginator state
