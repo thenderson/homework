0400d15761b47c1d8a256f7729da4aa6faf9eb34
thenderson <todd.hndrsn@gmail.com>
2014-11-23 10:08:07 -0800

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- templates/user_page_form.php
+++ templates/user_page_form.php
@@ -7,7 +7,7 @@
 				<img src="/commgr/public/img/BA Logo Blue 100.jpg" alt="BA logo">
 			</div>
 			<div class="col-sm-10">
-				<h2>Welcome, <?php=$_SESSION["username"]?></h2>
+				<h2>Welcome, <?php$_SESSION["username"]?></h2>
 				<h4>here's your dashboard.</h4>
 			</div>
 		</div>
@@ -15,34 +15,7 @@
 <br>
 <br>
 <div class="container">		
-	<form class="form-horizontal" action="login.php" method="post" role="form">
-		<fieldset>
-			<div class="row">
-				<legend>
-					<div class="col-sm-10 col-sm-offset-2">
-						<h5>please log in or <a href="register.php">register</a> for an account.</h5>
-					</div>
-				</legend>
-			</div>
-			<div class="form-group">
-				<label for="username" class="col-sm-3 control-label">username</label>
-				<div class="col-sm-4">
-					<input autofocus class="form-control" name="username" id="username" type="text"/>
-				</div>
-			</div>
-			<div class="form-group"> 
-				<label for="password" class="col-sm-3 control-label">password</label>
-				<div class="col-sm-4">
-					<input class="form-control" name="password" id="password" type="password"/>
-				</div>
-			</div>
-			<div class="form-group">
-				<div class="col-sm-offset-3 col-sm-9">
-					<button type="submit" class="btn btn-default">go</button>
-				</div>
-			</div>
-		</fieldset>
-	</form>
+	
 	<br>
 </div><!--close container-->
 <br>
