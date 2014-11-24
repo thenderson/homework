6f47b930c54298a6f97573c19afb257a4a22631c
thenderson <todd.hndrsn@gmail.com>
2014-11-23 20:13:32 -0800

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/load_data.php
+++ public/load_data.php
@@ -58,7 +58,7 @@
 	$grid->addColumn('description', 'COMMITMENT', 'string');
 	$grid->addColumn('promiser','PROMISER','string', $username_lookup);
 	$grid->addColumn('requester','REQUESTER','string', $username_lookup);
-	$grid->addColumn('due_by','DUE BY','date');
+	$grid->addColumn('due_by_f','DUE BY','date');
 	$grid->addColumn('status','STATUS','string');
 	$grid->addColumn('metric','METRIC','string', NULL, false);
 	$grid->addColumn('actions', 'DO', 'html', NULL, false, 'id');
