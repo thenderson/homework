b743eb9897a045cf772b8389412db9842720fa8f
thenderson <todd.hndrsn@gmail.com>
2014-11-29 13:22:47 -0800

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- includes/load_user_commitments.php
+++ includes/load_user_commitments.php
@@ -56,7 +56,8 @@
 	
 	//declare grid columns TODO add columns for due/overdue, variance
 	$grid->addColumn('unique_id', 'U_ID #', 'integer', NULL, false);
-	$grid->addColumn('project_number', 'PROJECT #', 'string', $projects);
+	$grid->addColumn('project_number', 'PROJECT #', 'string');
+	$grid->addColumn('project_number', 'PROJECT NAME', 'string', $projects);
 	$grid->addColumn('task_id', 'ID #', 'string', NULL, false);
 	$grid->addColumn('description', 'COMMITMENT', 'string');
 	//$grid->addColumn('promiser','PROMISER','string', $username_lookup);
