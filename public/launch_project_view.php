14cfc1584969427541588a23d30f8eafcf8632bd
thenderson <todd.hndrsn@gmail.com>
2014-11-29 20:40:30 -0800

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/launch_project_view.php
+++ public/launch_project_view.php
@@ -1,9 +1,11 @@
 <?php
-    require("../includes/config.php");
+    require('../includes/config.php');
 	
-	if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['p']))
+	if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['p']))
     {		
 		echo 'ok';
-		render('commitments_formb.html', ["project" => $_POST['p']]);
+		render('commitments_form.html', ['project' => $_POST['p']]);
+		error_log('back from the render');
 	}
 	else echo 'fail';
+	error_log("this shouldnt happen");
