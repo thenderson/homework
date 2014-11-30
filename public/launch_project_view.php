456ab6c56df25b7304c0c8a23ec504c61004a297
thenderson <todd.hndrsn@gmail.com>
2014-11-29 19:51:25 -0800

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/launch_project_view.php
+++ public/launch_project_view.php
@@ -1,4 +1,9 @@
 <?php
     require("../includes/config.php");
-    render("commitments_form.html");
+	error_log("project view ".$_POST('p');
+	
+	if ($_SERVER["REQUEST_METHOD"] == "POST")
+    {		
+        // validate submission
+        if (!empty($_POST['p'])) render('commitments_form.html');
 ?>
