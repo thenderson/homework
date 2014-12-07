4b85a4aa936e46a5a52f466bc3cd921680728d66
thenderson <todd.hndrsn@gmail.com>
2014-12-06 21:20:08 -0800

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/launch_project_view.php
+++ public/launch_project_view.php
@@ -1,17 +1,20 @@
 <?php
     require('../includes/config.php');
 	
-	if ($_SERVER['REQUEST_METHOD'] == 'POST' && !empty($_POST['p']))
+	if ($_SERVER['REQUEST_METHOD'] == 'GET' && !empty($_GET['p']))
     {		
 		http_response_code(302);
 		//header($location);
 		header('Content-Type: text/html; charset=UTF-8');
 		header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
 		header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
-		render('commitments_form.html', ['project' => $_POST['p']]);
+		render('commitments_form.html', ['project' => $_GET['p']]);
 		error_log('back from the render');
 	}
-	else echo 'fail';
-	error_log("this shouldnt happen");
+	else 
+	{
+		echo 'fail';
+		error_log("this shouldn't happen");
+	}
 	
 	//consider changing request to GET instead of POST.
