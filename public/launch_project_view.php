54fd471044e0447a734c5bd6d7a603bbe895dd8a
thenderson <todd.hndrsn@gmail.com>
2014-12-06 22:38:02 -0800

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/launch_project_view.php
+++ public/launch_project_view.php
@@ -5,11 +5,11 @@
     {		
 		http_response_code(302);
 		//header($location);
-		header('Content-Type: text/html; charset=UTF-8');
-		header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
-		header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
+		//header('Content-Type: text/html; charset=UTF-8');
+		//header('Cache-Control: no-cache, must-revalidate'); // HTTP/1.1
+		//header('Expires: Sat, 26 Jul 1997 05:00:00 GMT'); // Date in the past
 		render('commitments_form.html', ['project' => $_GET['p']]);
-		error_log('back from the render');
+		error_log('ok...back from the render');
 	}
 	else 
 	{
