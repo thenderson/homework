ee46d3808e2e47347f0de4ffcc6e08079ba62535
thenderson <todd.hndrsn@gmail.com>
2015-01-01 15:55:37 -0800

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- includes/functions.php
+++ includes/functions.php
@@ -132,25 +132,25 @@
     function render($template, $values = [])
     {
         // if template exists, render it
-        if (file_exists("../templates/$template"))
+        if (file_exists("/templates/$template"))
         {
             // extract variables into local scope
             extract($values);
 
             // render header
-            require('../templates/header.html');
+            require('/templates/header.html');
 
             // render template
-            require("../templates/$template");
+            require("/templates/$template");
 
             // render footer
-            require('../templates/footer.php');
+            require('/templates/footer.php');
         }
 
         // else err
         else
         {
-            trigger_error("Ooh! Invalid template: $template", E_USER_ERROR);
+            trigger_error("Invalid template: $template", E_USER_ERROR);
         }
     }
 
