eedd28557b14a42b98f04d366a5c7d26958cf7db
thenderson <todd.hndrsn@gmail.com>
2015-01-02 21:24:21 -0800

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/js/editablegrid_utils.js
+++ public/js/editablegrid_utils.js
@@ -214,7 +214,9 @@ EditableGrid.prototype.autoWidth = function (element)
  */
 EditableGrid.prototype.autoHeight = function (element) 
 {
-	console.log('offsetHeight: '+element.offsetHeight+' paddingTop: '+this.paddingTop(element)+' paddingBottom: '+this.paddingBottom(element)+' borderTop: '+this.borderTop(element)+' borderBottom: '+this.borderBottom(element));
+	console.log('offsetHeight: '+element.offsetHeight);
+	console.log('paddingTop: '+this.paddingTop(element)+' paddingBottom: '+this.paddingBottom(element));
+	console.log('borderTop: '+this.borderTop(element)+' borderBottom: '+this.borderBottom(element));
 	return element.offsetHeight - this.paddingTop(element) - this.paddingBottom(element) - this.borderTop(element) - this.borderBottom(element);
 };
 
