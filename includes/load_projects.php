e64858491a616d37a3db7aa96398b8ef7e5564e4
thenderson <todd.hndrsn@gmail.com>
2015-01-11 22:42:57 -0800

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

---
+++ includes/load_projects.php
@@ -0,0 +1,41 @@
+<?php
+
+    // configuration
+    require_once('../includes/config.php');     
+	require_once('../includes/EditableGrid.php');     
+
+	/*	RETRIEVE PROJECT LIST */
+	
+	$stmt = $comm_db->prepare("
+		SELECT project_number as project_number_2, project_name
+		FROM projects
+		NATURAL JOIN users_projects
+		WHERE user_id = ?
+		ORDER BY project_number_2");
+	
+	if (!$stmt)
+	{
+		trigger_error('Statement failed : ' . $stmt->error, E_USER_ERROR);
+		exit;
+	}
+	
+	try 
+	{
+		$stmt->bindParam(1, $_SESSION['id'], PDO::PARAM_INT);
+		$stmt->execute();		
+		$commitments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	} 
+	catch(PDOException $e) 
+	{
+		trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $e->getMessage(), E_USER_ERROR);
+	}
+		
+	// create grid
+	$grid = new EditableGrid();
+	
+	//declare grid columns TODO add columns for PPC & TA
+	$grid->addColumn('project_number_2', 'PROJECT #', 'string', NULL, false);
+	$grid->addColumn('project_name', 'PROJECT NAME', 'string', NULL, false);
+
+	//render grid
+	$grid->renderXML($commitments);
