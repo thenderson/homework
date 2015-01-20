0f746198f371b9052b022d5ecc543ff48184b174
thenderson <todd.hndrsn@gmail.com>
2015-01-19 23:38:37 -0800

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- includes/load_user_commitments.php
+++ includes/load_user_commitments.php
@@ -10,7 +10,7 @@
 
 	/*  COMPOSE QUERY */
 	$q = "SELECT unique_id, project_number, task_id, description, requester, promiser, DATE_FORMAT(due_by,'%m/%d/%Y') as due_by, 
-		priority_h, status, status NOT IN ('O', '?', 'D', 'NA', NULL) as is_closed FROM commitments";
+		priority_h, status, IF(status IN ('O', '?', 'D', 'NA', NULL),0,1) as is_closed FROM commitments";
 	
 	if ($planning_horizon == 'all') $q = $q . " WHERE promiser = :promiser";
 	else $q = $q . " WHERE due_by <= DATE_ADD(CURDATE(), INTERVAL :horizon DAY) and promiser = :promiser";
