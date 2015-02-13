b10a270ecd524c85c6840776fe2d21005128386d
thenderson <todd.hndrsn@gmail.com>
2015-02-12 23:13:23 -0800

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/js/reliable.js
+++ public/js/reliable.js
@@ -82,8 +82,8 @@ function CommitmentGrid()
 					row=self.grid.getRow(cell.rowIndex);
 					status=self.grid.getValueAt(cell.rowIndex, 8);
 					how_soon=date_due.diff(moment(),'days');
-					console.log('status'+status);
-					if (status == true) {
+					console.log('status '+status);
+					if (status === true) {
 						$(row).addClass('closed');
 					}
 					else {
