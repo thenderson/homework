e7c51306bb132ee6af21fc2c52dc84b52171c7ca
thenderson <todd.hndrsn@gmail.com>
2015-03-17 20:28:10 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- includes/commitment_update.php
+++ includes/commitment_update.php
@@ -79,8 +79,7 @@ switch ($column_name) {
 		$foresight = date_diff($requested_on, $due)->format('d');
 		$now = new DateTime();
 		$when_due = date_diff($due, $now)->format('d');
-		
-		$new_value = ($when_due > 13) ? 'C2' : (($when_due > 6) ? 'C1' : 'C0');
+		$new_value = $when_due < 0 ? 'CL': ($foresignt > 13 ? 'C2' : ($foresight > 6 ? 'C1' : 'C0'));
 		
 		$q="UPDATE commitments SET status = ? WHERE unique_id = ?";
 		break;
