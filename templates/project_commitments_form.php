803cf671a8a2946cf638a7805b9963e5e6b2d538
thenderson <todd.hndrsn@gmail.com>
2015-03-23 23:02:31 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- templates/project_commitments_form.php
+++ templates/project_commitments_form.php
@@ -29,67 +29,67 @@
 	</body>	
 <?php require 'footer.php'; ?>
 
-	<script type="text/javascript">
-		jQuery.datepicker.setDefaults({dateFormat:$.datepicker.W3C});   
+<script type="text/javascript">
+	jQuery.datepicker.setDefaults({dateFormat:$.datepicker.W3C});   
+	
+	commitments = new CommitmentGrid();
+	commitments.name = "project_commitments";
+	
+	window.onload = function() {
+		pnum = getparam('project');
 		
-		commitments = new CommitmentGrid();
-		commitments.name = "project_commitments";
+		// populate new commitment requester and promiser select menus with project team members
+		$.ajax({
+			url: '../includes/load_project_usernames.php',
+			type: 'POST',
+			dataType: 'JSON',
+			data: { p: getparam('project') },
+			success: function (proj_users) {
+				populate_select_names("#inp-req", proj_users);
+				populate_select_names("#inp-prom", proj_users);
+			},
+			error: function(XMLHttpRequest, textStatus, exception) { 
+				alert("Ajax FAIL!\n" + "\nTextstatus: " + textStatus + "\nException: " + exception);},
+			async: true
+		});
 		
-		window.onload = function() {
-			pnum = getparam('project');
-			
-			// populate new commitment requester and promiser select menus with project team members
-			$.ajax({
-				url: '../includes/load_project_usernames.php',
-				type: 'POST',
-				dataType: 'JSON',
-				data: { p: getparam('project') },
-				success: function (proj_users) {
-					populate_select_names("#inp-req", proj_users);
-					populate_select_names("#inp-prom", proj_users);
-				},
-				error: function(XMLHttpRequest, textStatus, exception) { 
-					alert("Ajax FAIL!\n" + "\nTextstatus: " + textStatus + "\nException: " + exception);},
-				async: true
-			});
-			
-			//load project name, populate table_title & project number selectmenu in New Commitment dialog
-			$.ajax({
-				url: '../includes/load_project_name.php',
-				type: 'POST',
-				dataType: 'text',
-				data: { p: pnum },
-				success: function (response) {
-					$('#table_title').html("<h3><strong>PROJECT COMMITMENTS: "+response+"</strong> | #"+pnum+"</h3>");
-					$('#inp-proj').append($("<option>").attr('value',response).text(response)).selectmenu('disable');
-				},
-				error: function(XMLHttpRequest, textStatus, exception) { 
-					alert("Ajax FAIL!\n" + "\nTextstatus: " + textStatus + "\nException: " + exception);},
-				async: true
-			});
-			
-			$("#filter_all").keyup(function() { commitments.grid.filter($(this).val(), [1,2,3,4,5,6]); });
-			
-			$.ajax({
-				url: '../includes/load_project_commitments.php',
-				type: 'POST',
-				dataType: "text",
-				data: {
-					horizon: 30,
-					p: getparam('project')
-				},
-				success: function (response) 
-				{
-					commitments.grid.loadXMLFromString(response); //synchronous function
-					commitments.grid.tableLoaded();
-				},
-				error: function(XMLHttpRequest, textStatus, exception) 
-				{ 
-					alert("Ajax FAIL!\n" + "\nTextstatus: " + textStatus + "\nException: " + exception); 
-				},
-				async: true
-			});
+		//load project name, populate table_title & project number selectmenu in New Commitment dialog
+		$.ajax({
+			url: '../includes/load_project_name.php',
+			type: 'POST',
+			dataType: 'text',
+			data: { p: pnum },
+			success: function (response) {
+				$('#table_title').html("<h3><strong>PROJECT COMMITMENTS: "+response+"</strong> | #"+pnum+"</h3>");
+				$('#inp-proj').append($("<option>").attr('value',response).text(response)).selectmenu('disable');
+			},
+			error: function(XMLHttpRequest, textStatus, exception) { 
+				alert("Ajax FAIL!\n" + "\nTextstatus: " + textStatus + "\nException: " + exception);},
+			async: true
+		});
+		
+		$("#filter_all").keyup(function() { commitments.grid.filter($(this).val(), [1,2,3,4,5,6]); });
+		
+		$.ajax({
+			url: '../includes/load_project_commitments.php',
+			type: 'POST',
+			dataType: "text",
+			data: {
+				horizon: 30,
+				p: getparam('project')
+			},
+			success: function (response) 
+			{
+				commitments.grid.loadXMLFromString(response); //synchronous function
+				commitments.grid.tableLoaded();
+			},
+			error: function(XMLHttpRequest, textStatus, exception) 
+			{ 
+				alert("Ajax FAIL!\n" + "\nTextstatus: " + textStatus + "\nException: " + exception); 
+			},
+			async: true
+		});
 
-			//$(function () { $("[data-toggle='tooltip']").tooltip(); });
-		};
-	</script>
+		//$(function () { $("[data-toggle='tooltip']").tooltip(); });
+	};
+</script>
