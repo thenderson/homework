bf97987d216dbbe801bf238f38e30f47a13705db
thenderson <todd.hndrsn@gmail.com>
2015-03-26 20:47:51 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/js/reliable.js
+++ public/js/reliable.js
@@ -102,14 +102,6 @@ function CommitmentGrid(name)
 					else $(cell).removeClass('priority-h');
 				}}));
 			
-			// this.setCellRenderer('status', new CellRenderer ({ //overdue rows
-				// render: function(cell, value) {
-					// if (value == 'V?' || value == '?') {
-						// $(row).addClass('status_me_now');
-					// }
-				// }
-			// }));
-			
 			this.setEnumProvider('status', new EnumProvider({
 				getOptionValuesForEdit: function (grid, column, rowIndex) {
 					var status = editableGrid.getValueAt(rowIndex, column);
@@ -120,6 +112,15 @@ function CommitmentGrid(name)
 						'V5':'variance - superseded, internal','V6':'variance - superseded, external', 
 						'V7':'variance - forgot','V8':'variance - not needed','V9':'variance - tech failure','V?':'variance - unknown'};
 					return status;
+				}
+			}));
+				
+			this.setCellRenderer('status', new CellRenderer ({ //overdue rows
+				render: function(cell, value) {
+					cell.innerHTML = value;
+					if (value == 'V?' || value == '?') {
+						$(row).addClass('status_me_now');
+					}
 				}
 			}));
 			
