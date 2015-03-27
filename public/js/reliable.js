9d7dc6847a3f67e004bf53aa0ebb7a1f7bd79eab
thenderson <todd.hndrsn@gmail.com>
2015-03-26 22:43:36 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/js/reliable.js
+++ public/js/reliable.js
@@ -104,12 +104,13 @@ function CommitmentGrid(name)
 				}}));
 			
 			this.setEnumProvider('status', new EnumProvider({
-				getOptionValuesForRender: function (grid, column, rowIndex) {
+				getOptionValuesForEdit: function (grid, column, rowIndex) {
 					status = self.grid.getValueAt(rowIndex, status_col);
 					console.log('rowindex '+rowIndex+' column '+status_col+' status: '+ status);
 					if (status == 'O') return { 'O' : 'O', 'C' : 'close', 'D':'defer', '?':'unknown'};
-					else if (status == 'V?') return { 'V1':'1 time','V2':'2 waiting, internal','V3':'3 waiting, external','V4':'4 COS','V5':'5 superseded, internal','V6':'6 superseded, external','V7':'7 forgot','V8':'8 not needed','V9':'9 tech failure','V?':'unknown'};
-					return { 'O' : 'open', 'C' : 'close', 'D':'defer', '?':'unknown'};
+					else if (status == 'V?') return { 'V1':'1 time','V2':'2 waiting, int.','V3':'3 waiting, ext.','V4':'4 COS','V5':'5 superseded, int.','V6':'6 superseded, ext.','V7':'7 forgot','V8':'8 not needed','V9':'9 tech failure','V?':'V?
+					'};
+					return;
 				}
 			}));
 				
