a03ce96d454937aec7b495bf9ee1bce7abc7bff5
thenderson <todd.hndrsn@gmail.com>
2015-03-28 21:35:45 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- includes/commitment_update.php
+++ includes/commitment_update.php
@@ -85,7 +85,7 @@ switch ($column_name) {
 				case 'O':
 					if ($new_value == 'C') {
 						// 1. open --> closed: calculate closing status value; increment PPC & TA to project & individual
-						$new_value = calc_closed_status($unique_id, $date_due);
+						$new_value = calc_closed_status($unique_id, $date_due, $comm_db);
 						$q='UPDATE commitments SET status = ? WHERE unique_id = ?';
 						$promiser_q = $comm_db->query("SELECT promiser FROM commitments WHERE unique_id = $unique_id");
 						
@@ -180,7 +180,7 @@ switch ($column_name) {
 					}
 					else if ($new_value == 'C') {
 						// 8. unknown --> closed: calculate closing status value; increment PPC & TA to project & individual
-						$new_value = calc_closed_status($unique_id, $date_due);
+						$new_value = calc_closed_status($unique_id, $date_due, $comm_db);
 						$q='UPDATE commitments SET status = ? WHERE unique_id = ?';
 						
 						$promiser_q = $comm_db->query("SELECT promiser FROM commitments WHERE unique_id = $unique_id");
@@ -321,9 +321,9 @@ exit;
 
 // functions
 
-function calc_closed_status($id, $duedate) {
+function calc_closed_status($id, $duedate, $dbase) {
 		// lookup when the commitment was first requested
-	$s = $comm_db->prepare("SELECT requested_on FROM commitments WHERE unique_id = ?");
+	$s = $dbase->prepare("SELECT requested_on FROM commitments WHERE unique_id = ?");
 
 	if (!$s) {
 		trigger_error('Statement failed : ' . $q->error, E_USER_ERROR);
