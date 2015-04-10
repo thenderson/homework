a9df10b9f4f7a5a3b88e2561131cc93c57d05f56
thenderson <todd.hndrsn@gmail.com>
2015-04-09 22:35:29 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- includes/load_user_commitments.php
+++ includes/load_user_commitments.php
@@ -13,13 +13,11 @@
 	$q = "SELECT unique_id, project_number, task_id, description, requester, promiser, due_by, 
 		priority_h, status, IF(status IN ('O', '?', 'D', 'NA', 'V?'),0,1) as is_closed FROM commitments";
 	
-	if ($planning_horizon == 'all')
-	{
+	if ($planning_horizon == 'all') {
 		if ($p_or_r == 'promises') $q = $q . ' WHERE promiser = :user';
 		else $q = $q . ' WHERE requester = :user';
 	}
-	else 
-	{
+	else {
 		if ($p_or_r == 'promises') $q = $q . " WHERE due_by <= DATE_ADD(CURDATE(), INTERVAL :horizon DAY) and promiser = :user";
 		else $q = $q . " WHERE due_by <= DATE_ADD(CURDATE(), INTERVAL :horizon DAY) and requester = :user";
 	}
@@ -31,29 +29,25 @@
 	/*	RETRIEVE COMMITMENTS */
 	$stmt = $comm_db->prepare($q);
 	
-	if (!$stmt)
-	{
+	if (!$stmt) {
 		trigger_error('Statement failed: ' . $stmt->error, E_USER_ERROR);
 		exit;
 	}
 	
-	try 
-	{
+	try {
 		if ($planning_horizon != 'all') $stmt->bindParam(':horizon', $planning_horizon, PDO::PARAM_INT);
 		$stmt->bindParam(':user', $_SESSION['id'], PDO::PARAM_INT);
 		$stmt->execute();		
 		$commitments = $stmt->fetchAll(PDO::FETCH_ASSOC);
 	} 
-	catch(PDOException $e) 
-	{
+	catch(PDOException $e) {
 		trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $e->getMessage(), E_USER_ERROR);
 	}
 	
 	/*	RETRIEVE USER_ID & NAMES */ //move this to config & pass into this script?
 	$user_res = $comm_db->query("SELECT user_id, name FROM users ORDER BY email ASC");
 	if (!$user_res) trigger_error('Statement failed : ' . E_USER_ERROR);
-	else 
-	{
+	else {
 		$users = $user_res->fetchAll(PDO::FETCH_ASSOC);
 		foreach ($users as $row) $username_lookup[$row["user_id"]] = $row["name"];
 	}
@@ -61,10 +55,10 @@
 	/*	RETRIEVE PROJECT NUMBERS & PROJECT SHORTNAMES */ //move this to config & pass into this script?
 	$proj_res = $comm_db->query("SELECT project_number, project_shortname FROM projects");
 	if (!$proj_res) trigger_error('Statement failed : ' . E_USER_ERROR);
-	else 
-	{
+	else {
 		$rows = $proj_res->fetchAll(PDO::FETCH_ASSOC);
 		foreach ($rows as $row) $projects[$row["project_number"]] = $row["project_shortname"];
+var_dump($projects);
 		foreach ($commitments as $commitment) $commitment['project_shortname'] = $projects[$commitment['project_number']];
 	}
 
