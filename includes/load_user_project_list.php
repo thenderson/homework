286434aa8ff455338914b3248c8f774ba57c4001
thenderson <todd.hndrsn@gmail.com>
2015-04-18 20:11:16 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- includes/load_user_project_list.php
+++ includes/load_user_project_list.php
@@ -51,7 +51,8 @@
 	$last_monday = new DateTime(date('Y-m-d', strtotime('last Monday')));
 
 	foreach ($rows as $row) {
-		$weeknum = date_diff($last_monday, strtotime($row['date']))->format('%r%a') / 7;
+		$date = new DateTime(date('Y-m-d', $row['date']));
+		$weeknum = date_diff($last_monday, $date) / 7;
 error_log($weeknum);
 		$project_metrics[$row['project_number']]['PPC'][$weeknum] = $row['PPC'];
 		$project_metrics[$row['project_number']]['PTA'][$weeknum] = $row['PTA'];
