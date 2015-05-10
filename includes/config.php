624e06e0dbb1ffe9e849cbd6c2482095acb694c6
thenderson <todd.hndrsn@gmail.com>
2015-05-09 18:33:22 -0700

    lengthening inactive time before re-loggin fixing new commitment modal by emptying selectmenus
    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- includes/config.php
+++ includes/config.php
@@ -20,8 +20,8 @@
 	// manage existing sessions as recommended by Gumbo @ 
 	// http://stackoverflow.com/questions/520237/how-do-i-expire-a-php-session-after-30-minutes/1270960#1270960
 	
-	if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 14400)) {
-    // last request was more than 4 hours ago
+	if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 28800)) {
+    // last request was more than 8 hours ago
     session_unset();     // unset $_SESSION variable for the run-time 
     session_destroy();   // destroy session data in storage
 	}
@@ -30,8 +30,8 @@
 	if (!isset($_SESSION['CREATED'])) {
 		$_SESSION['CREATED'] = time();
 	} 
-	else if (time() - $_SESSION['CREATED'] > 3600) {
-		// session started more than 60 minutes ago
+	else if (time() - $_SESSION['CREATED'] > 7200) {
+		// session started more than 120 minutes ago
 		session_regenerate_id(true);    // change session ID for the current session and invalidate old session ID
 		$_SESSION['CREATED'] = time();  // update creation time
 	}
