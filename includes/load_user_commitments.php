c5fb94ce102e99efb5f08ad9588f934f495e681f
thenderson <todd.hndrsn@gmail.com>
2015-05-12 21:32:55 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- includes/load_user_commitments.php
+++ includes/load_user_commitments.php
@@ -19,11 +19,13 @@
 		trigger_error('Statement failed: ' . $stmt->error, E_USER_ERROR);
 		exit;
 	}
-	
 	try {
 		$stmt->bindParam(':user', $_SESSION['id'], PDO::PARAM_INT);
 		$stmt->execute();		
 		$preferences = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
+	catch(PDOException $e) {
+		trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $e->getMessage(), E_USER_ERROR);
 	}
 	
 	/*  COMPOSE QUERY */
