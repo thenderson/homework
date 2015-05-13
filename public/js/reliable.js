331aab450ff48e95885797b898acd2ae2dff1e5c
thenderson <todd.hndrsn@gmail.com>
2015-05-12 20:29:11 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/js/reliable.js
+++ public/js/reliable.js
@@ -82,8 +82,8 @@ function CommitmentGrid(name) {
 				this.setCellRenderer('project_number', new CellRenderer({ 
 					render: function(cell, value) { 
 						cell.innerHTML= "<a title=\"go to project page\" href=\"#\" onclick=\"goto_project_view(\'"+value+"\'); return false;\">"+value+"</a>";
-console.log(self.sortedColumnName);
-						if (self.sortedColumnName == 'project_number') {	
+console.log(self.grid.sortedColumnName);
+						if (self.grid.sortedColumnName == 'project_number') {	
 							if (getValueAt(cell.rowIndex-1, pnum_col) != value && cell.rowIndex>1) {
 								row=self.grid.getRow(cell.rowIndex);
 								row.addClass('border-dark');
