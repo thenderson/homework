dbfc3c41b18c5b31d25591865108a8cd022249c9
thenderson <todd.hndrsn@gmail.com>
2015-05-13 00:30:03 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- includes/update_user_preferences.php
+++ includes/update_user_preferences.php
@@ -32,7 +32,7 @@ try {
 	$prefs = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 catch(PDOException $e) {
-	trigger_error('Error: ' . $e->getMessage(), E_USER_ERROR);
+	trigger_error('Wrong SQL: ' . $q . ' Error: ' . $e->getMessage(), E_USER_ERROR);
 }
 
 echo 'ok';
