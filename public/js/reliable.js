61c2919b373cc6061a15968e37161c4c88a65140
thenderson <todd.hndrsn@gmail.com>
2015-09-02 21:28:21 -0700

    Signed-off-by: thenderson <todd.hndrsn@gmail.com>

--- public/js/reliable.js
+++ public/js/reliable.js
@@ -157,7 +157,7 @@ function CommitmentGrid(name) {
 					else if (status == '?') return {'O':'reOpen', 'C':'Close', 'D':'Defer'};
 					else if (/C[L012]/.test(status)) return {'O':'reOpen'}; 
 					else if (status == 'V?') return {'V1':'V1 time','V2':'V2 waiting, int.','V3':'V3 waiting, ext.','V4':'V4 COS','V5':'V5 fire, int.',
-						'V6':'V6 fire, ext.','V7':'V7 forgot','V8':'V8 not needed','V9':'V9 tech failure','C':'Close'};
+						'V6':'V6 fire, ext.','V7':'V7 forgot','V8':'V8 not needed','V9':'V9 tech failure','CC':'Closed on time'};
 					else if (/V[123456789*]/.test(status)) return {'V1':'V1 time','V2':'V2 waiting, int.','V3':'V3 waiting, ext.','V4':'V4 COS','V5':'V5 fire, int.',
 						'V6':'V6 fire, ext.','V7':'V7 forgot','V8':'V8 not needed','V9':'V9 tech failure'};
 					return;
